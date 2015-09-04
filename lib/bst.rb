4ef98b2c0243a5de0c5d63a70398851669621cb1
ShannonPaige <spaige@gmail.com>
2015-09-03 18:29:05 -0600

    Added insert and TDD tests

--- lib/bst.rb
+++ lib/bst.rb
@@ -12,18 +12,26 @@ class BST
     @head == NullNode::DEFAULT
   end
 
-  def insert(node_value)
+  def insert(node_value, here = @head)
     if bst_empty?
       @head = Node.new(node_value)
     else
-    here = @head
-    new_node = Node.new(node_value) # => #<Node:0x007f9871917f60 @data="b", @left=#<NullNode:0x007f987191d118>, @right=#<NullNode:0x007f987191d118>>
-    if new_node.data < here.data
-      here.left = new_node
-    else
-      here.right = new_node
-    end 
-  end
+      new_node = Node.new(node_value)
+      if new_node.data < here.data && here.left == NullNode::DEFAULT
+        here.left = new_node
+        return
+      elsif new_node.data > here.data && here.right == NullNode::DEFAULT
+        here.right = new_node
+        return
+      elsif new_node.data < here.data
+        here = here.left
+        insert(node_value, here)
+      else
+        here = here.right
+        insert(node_value, here)
+      end
+    end
+    self # => #<BST:0x007f9bd404ba38 @head=#<Node:0x007f9bd404b790 @data="d", @left=#<NullNode:0x007f9bd4050830>, @right=#<NullNode:0x007f9bd4050830>>>
   end
 
   def include?
@@ -59,12 +67,13 @@ class BST
   end
 end
 
-tree = BST.new
-tree.insert("d")
-tree.insert("b")
-#               .insert("a")
-#               .insert("c")
-#             .insert("f")
-#               .insert("e")
-#               .insert("g")
-tree # => #<BST:0x007f987191c010 @head=#<Node:0x007f9871917fb0 @data="d", @left=#<Node:0x007f9871917f60 @data="b", @left=#<NullNode:0x007f987191d118>, @right=#<NullNode:0x007f987191d118>>, @right=#<NullNode:0x007f987191d118>>>
+tree = BST.new # => #<BST:0x007f9bd404ba38 @head=#<NullNode:0x007f9bd4050830>>
+tree.insert("d") # => #<BST:0x007f9bd404ba38 @head=#<Node:0x007f9bd404b790 @data="d", @left=#<NullNode:0x007f9bd4050830>, @right=#<NullNode:0x007f9bd4050830>>>
+# tree.insert("b") # => nil
+# tree.insert("a")
+# tree.insert("c") # => #<BST:0x007fe0118429e0 @head=#<Node:0x007fe011842760 @data="d", @left=#<Node:0x007fe011842120 @data="b", @left=#<Node:0x007fe011841ec8 @data="a", @left=#<NullNode:0x007fe0118433b8>, @right=#<NullNode:0x007fe0118433b8>>, @right=#<Node:0x007fe011841a18 @data="c", @left=#<NullNode:0x007fe0118433b8>, @right=#<NullNode:0x007fe0118433b8>>>, @right=#<NullNode:0x007fe0118433b8>>>
+# tree.insert("f")
+# tree.insert("e")
+# tree.insert("g")
+tree # => #<BST:0x007f9bd404ba38 @head=#<Node:0x007f9bd404b790 @data="d", @left=#<NullNode:0x007f9bd4050830>, @right=#<NullNode:0x007f9bd4050830>>>
+tree.head.data # => "d"
--- test/bst_test.rb
+++ test/bst_test.rb
@@ -1,15 +1,35 @@
 require "bst"
-#
-# class BST < MiniTest::Test
-#
-#   def setup
-#     @new_list = BST.new
-#     @new_node = Node.new(:node_data)
-#     @new_node2 = Node.new(:node2_data)
-#   end
-#
-#   def test_node_is_the_tail
-#     assert_equal nil, @new_node.next
-#   end
-#
-# end
+require "node"
+
+class BSTTest < MiniTest::Test
+
+  def setup
+    @new_list = BST.new
+    @new_node = Node.new(:node_data)
+    @new_node2 = Node.new(:node2_data)
+  end
+
+  def test_insert_adds_a_node_to_the_head_of_an_empty_binary_tree
+    @new_list.insert("d")
+    assert_equal "d", @new_list.head.data
+    assert_equal NullNode::DEFAULT, @new_list.head.left
+    assert_equal NullNode::DEFAULT, @new_list.head.right
+  end
+
+  def test_insert_adds_a_node_to_the_left_of_a_larger_node
+    @new_list.insert("d")
+    @new_list.insert("b")
+    @new_list.insert("a")
+    assert_equal "b", @new_list.head.left.data
+    assert_equal "a", @new_list.head.left.left.data
+  end
+
+  def test_insert_adds_a_node_to_the_right_of_a_smaller_node
+    @new_list.insert("d")
+    @new_list.insert("f")
+    @new_list.insert("g")
+    assert_equal "f", @new_list.head.right.data
+    assert_equal "g", @new_list.head.right.right.data
+  end
+
+end
